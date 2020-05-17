073ad2cdf6dcfd9b1d0f7036251d16b5296272fb
Conor Shields <42182485+cshields236@users.noreply.github.com>
2020-05-17 01:35:15 +0100

    added survey fields

--- SureyApplication/Survey/src/Survey.java
+++ SureyApplication/Survey/src/Survey.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class Survey {
    
+    private String name;
+    private ArrayList <Question>  questions =new ArrayList <Question>(); 
+
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return this.questions;
+    }
+
+    public void setQuestions(ArrayList<Question> questions) {
+        this.questions = questions;
+    }
+
 }
