9ba6ce01f954dd8adb0204862bbde8f2c5a7368f
Conor Shields <42182485+cshields236@users.noreply.github.com>
2020-05-17 01:51:33 +0100

    created first test creating an empty survey

---
+++ SureyApplication/Survey/src/Controller.java
@@ -0,0 +1,8 @@
+public class Controller {
+
+    public Survey createEmptySurvey(){
+        Survey s = new Survey();
+        return s;
+    }
+    
+}
--- SureyApplication/Survey/src/Tester.java
+++ SureyApplication/Survey/src/Tester.java
@@ -1,3 +1,16 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class Tester {
     
+    // Create controller object
+    Controller controller = new Controller();
+
+    @Test
+    public void TestEmptySurveyCreation(){
+        Survey s = controller.createEmptySurvey();
+
+        assertTrue("Should create a survey object",  s instanceof Survey);
+    }
 }
