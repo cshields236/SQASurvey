20c59dd149c1180df288b5618cab526d6ba047de
Conor Shields <42182485+cshields236@users.noreply.github.com>
2020-05-17 01:26:22 +0100

    add fields to question class

--- SureyApplication/Survey/src/Question.java
+++ SureyApplication/Survey/src/Question.java
@@ -1,4 +1,38 @@
 public class Question {
     
+    private String question;
+    private int answer;
+    // Question Constructers
+    public Question() {
+    }
+
+    public Question question(String question, int answer) {
+        this.question = question;
+        this.answer = answer;
+        return this;
+    }
+
+
+    //Question getter and setter 
+
+    public String getQuestion() {
+        return this.question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public int getAnswer() {
+        return this.answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+ 
+
+ 
+
     
 }
