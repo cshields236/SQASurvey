696eda7907e116dbeba700fcb2bf56bc6e59f9d5
Conor Shields <42182485+cshields236@users.noreply.github.com>
2020-05-18 00:56:21 +0100

    testing and adding creation of survey response

--- SureyApplication/Survey/src/Controller.java
+++ SureyApplication/Survey/src/Controller.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Controller {
 
@@ -7,10 +8,10 @@ public class Controller {
         return s;
     }
 
-    public Survey createSurvey(ArrayList<Question> surveyQuestions) {
+    public Survey createSurvey(String title, ArrayList<Question> surveyQuestions) {
 
         ArrayList<Question> questions = surveyQuestions;
-        Survey s = new Survey("Questions", questions);
+        Survey s = new Survey(title, questions);
         return s;
     }
 
@@ -20,27 +21,50 @@ public class Controller {
     }
 
     // Returning specific survey
-    public Survey retriveSpecificSurvey(ArrayList<Survey> surveys , String name){
-        
+    public Survey retriveSpecificSurvey(ArrayList<Survey> surveys, String name) {
+
         Survey returnSurvey = new Survey();
 
-        for(Survey s: surveys){
-            if (s.getName().equals(name)){
+        for (Survey s : surveys) {
+            if (s.getName().equals(name)) {
                 returnSurvey = s;
-                 
+
             }
-           
 
-    } return returnSurvey;
+        }
+        return returnSurvey;
 
     }
 
-
-      // Returning all surveys
-      public ArrayList<Survey> retriveAllSurveys(ArrayList<Survey> surveys ){
-         return surveys;
+    // Returning all surveys
+    public ArrayList<Survey> retriveAllSurveys(ArrayList<Survey> surveys) {
+        return surveys;
     }
 
-
+    // Create new survey response
+    public SurveyResponse createNewSurveyResponse(Survey survey, int response1, int response2, int response3) {
     
+        // Create empty surveyResponse object 
+        SurveyResponse surveyResponse = new SurveyResponse();
+        // Create Hashmap to be used for survey question answers 
+        HashMap<Integer, Integer> surveyAnswers = new HashMap<>();
+
+        
+        // Add the users reponses to hashmap
+        surveyAnswers.put(1,response1); 
+        surveyAnswers.put(2,response2); 
+        surveyAnswers.put(3,response3); 
+        
+        // Add the map with the users reponses to the survey response object
+        surveyResponse.setAnswers(surveyAnswers);
+        // Add the survey response to the survey
+        survey.addSurveyResponse(surveyResponse);
+        // return the survey response
+        return surveyResponse;
+
+  
+
+ 
+    }
+
 }
--- SureyApplication/Survey/src/Tester.java
+++ SureyApplication/Survey/src/Tester.java
@@ -17,83 +17,103 @@ public class Tester {
         assertTrue("Should create a survey object", s instanceof Survey);
     }
 
-    // test creating a survey with a question 
+    // test creating a survey with a question
     @Test
-    public void TestSurveyCreation(){
+    public void TestSurveyCreation() {
         Question q1 = new Question("Question One");
         ArrayList<Question> surveyQs = new ArrayList<Question>();
 
         surveyQs.add(q1);
 
-        Survey s = controller.createSurvey(surveyQs);
+        Survey s = controller.createSurvey("Test Survey", surveyQs);
 
-
-        assertTrue("Should create a survey object with question added", s.getQuestions().get(0).getQuestion().equals("Question One"));
+        assertTrue("Should create a survey object with question added",
+                s.getQuestions().get(0).getQuestion().equals("Question One"));
     }
 
-
-    @Test 
-	public void TestAddQuestion()
-	{
-		// Create Empty survey
+    @Test
+    public void TestAddQuestion() {
+        // Create Empty survey
         Survey s = controller.createEmptySurvey();
         // Create new question object
         Question one = new Question("Test Added Question");
         // call on method in controller class to add the question to the survey
         controller.addQuestion(s, one);
-		assertTrue("Question Should be added to survey", s.getQuestions().get(0).getQuestion().equals("Test Added Question"));
+        assertTrue("Question Should be added to survey",
+                s.getQuestions().get(0).getQuestion().equals("Test Added Question"));
     }
-    
 
     @Test
-    public void TestRetriveSpecificSurvey(){
-        //Create a series of surveys
+    public void TestRetriveSpecificSurvey() {
+        // Create a series of surveys
         Survey s1 = new Survey("Survey One");
         Survey s2 = new Survey("Survey Two");
         Survey s3 = new Survey("Survey Three");
         Survey s4 = new Survey("Survey Four");
-
 
         // add all these surveys to a collcection
-        ArrayList <Survey> surveys = new ArrayList <Survey>();
+        ArrayList<Survey> surveys = new ArrayList<Survey>();
 
         surveys.add(s1);
         surveys.add(s2);
         surveys.add(s3);
         surveys.add(s4);
 
-        // search for the specific survey 
+        // search for the specific survey
         Survey specificS = controller.retriveSpecificSurvey(surveys, "Survey One");
-        
-        assertTrue("Should return survey one", specificS.getName().equals("Survey One"));
 
+        assertTrue("Should return survey one", specificS.getName().equals("Survey One"));
 
     }
 
-    
     @Test
-    public void TestRetriveAll(){
-        //Create a series of surveys
+    public void TestRetriveAll() {
+        // Create a series of surveys
         Survey s1 = new Survey("Survey One");
         Survey s2 = new Survey("Survey Two");
         Survey s3 = new Survey("Survey Three");
         Survey s4 = new Survey("Survey Four");
 
-
         // add all these surveys to a collection
-        ArrayList <Survey> surveys = new ArrayList <Survey>();
-      
+        ArrayList<Survey> surveys = new ArrayList<Survey>();
+
         surveys.add(s1);
         surveys.add(s2);
         surveys.add(s3);
         surveys.add(s4);
 
-        // retrieve all the surveys created 
-        ArrayList <Survey> returnedSurveys = new ArrayList <Survey>();
+        // retrieve all the surveys created
+        ArrayList<Survey> returnedSurveys = new ArrayList<Survey>();
         returnedSurveys = controller.retriveAllSurveys(surveys);
-        
+
         assertTrue("Should return survey one", returnedSurveys.size() == 4);
 
+    }
+
+    @Test
+    public void TestAddSurveyResponse() {
+
+        // Add three questions to the survey
+        Question q1 = new Question("Test Added Question");
+        Question q2 = new Question("Test Added Question");
+        Question q3 = new Question("Test Added Question");
+        // Crate Questions list
+        ArrayList<Question> surveyQs = new ArrayList<Question>();
+
+        // Add Questions to list
+        surveyQs.add(q1);
+        surveyQs.add(q2);
+        surveyQs.add(q3);
+        // Create survey with questions
+        Survey s = controller.createSurvey("Example Survey ", surveyQs);
+        // Create answers for survey responses
+        int r1 = 3;
+        int r2 = 2;
+        int r3 = 4;
+        // call on method to create
+        SurveyResponse response = controller.createNewSurveyResponse(s, r1, r2, r3);
+        
+        assertTrue("Should return the survey response created" , response.getAnswers().size() == 3);
 
     }
 
