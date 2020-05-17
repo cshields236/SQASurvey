4d55ff4160b8c0341f429790e4217ab08a565020
Conor Shields <42182485+cshields236@users.noreply.github.com>
2020-05-17 23:23:23 +0100

    removed answer variable from question

--- SureyApplication/Survey/src/Question.java
+++ SureyApplication/Survey/src/Question.java
@@ -1,7 +1,6 @@
 public class Question {
     
     private String question;
-    private int answer;
     // Question Constructers
     public Question() {
     }
@@ -22,22 +21,7 @@ public class Question {
         this.question = question;
     }
 
-    public int getAnswer() {
-        return this.answer;
-    }
-
-    public void setAnswer(int answer) {
-
-        // Check to see that number is valid, if not thorw an exception
-        if (answer > 0 &&  answer <= 5){
-
-            this.answer = answer;
-        }
-        else {
-            throw new IllegalArgumentException("Answer for question must be between 1 & 5");
-        }
-       
-    }
+ 
  
 
  
